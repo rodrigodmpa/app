08c0038b7c59333fae7c25e49e045b76413a8678
Rodrigo Demetrio <rodrigo.demetrio.palma@gmail.com>
2018-07-14 17:37:31 -0300

    add admin on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!([
+    # Admin User
+    { 
+        :email => 'admin@admin.admin', 
+        :password => 'adminseed', 
+        :password_confirmation => 'adminseed',
+        :is_admin => true
+    }   
+])
