890b53f41bb04d22b5d214350f1687d562f88699
Rodrigo Demetrio <rodrigo.demetrio.palma@gmail.com>
2018-07-17 00:00:45 -0300

    create e read de sites

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,29 @@
 class SitesController < ApplicationController
+    def index
+        @sites = Site.all
+    end
+    def show
+        @site = Site.find(params[:id])
+    end
+
     def new
+        @site = Site.new
+    end
+
+    def create
+        @site = Site.new(site_params)
+        @site.user_id = current_user.id
+
+        if @site.save
+            redirect_to @site
+        else
+            flash
+            render 'new'
+        end
+    end
+
+    private
+    def site_params
+        params.require(:site).permit(:title, :text)
     end
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,4 @@
 class Site < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: { minimum: 5 }
 end
