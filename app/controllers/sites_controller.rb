2651a04f6d48ea32fd94ba7ad9c4bbddf82c607d
Rodrigo Demetrio <rodrigo.demetrio.palma@gmail.com>
2018-07-17 00:22:02 -0300

    feature concluida a principio

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,6 +1,6 @@
 class SitesController < ApplicationController
     def index
-        @sites = Site.all
+        @sites = Site.where("user_id = ?", current_user.id) # pega todos os sites que o usuario logado criou
     end
     def show
         @site = Site.find(params[:id])
@@ -9,6 +9,10 @@ class SitesController < ApplicationController
     def new
         @site = Site.new
     end
+
+    def edit
+        @site = Site.find(params[:id])
+      end
 
     def create
         @site = Site.new(site_params)
@@ -21,6 +25,16 @@ class SitesController < ApplicationController
             render 'new'
         end
     end
+
+    def update
+        @site = Site.find(params[:id])
+       
+        if @site.update(site_params)
+          redirect_to @site
+        else
+          render 'edit'
+        end
+      end
 
     private
     def site_params
