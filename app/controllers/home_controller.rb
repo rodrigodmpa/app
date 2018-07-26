236640f3ab084804b12e689d6f9b37c5ae737901
Rodrigo Demetrio <rodrigo.demetrio.palma@gmail.com>
2018-07-25 22:01:41 -0300

    add formatos de calendarios

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @sites = Site.where("user_id = ?", current_user.id) # pega todos os sites que o usuario logado criou
+    if user_signed_in?
+      @sites = Site.where("user_id = ?", current_user.id) # pega todos os sites que o usuario logado criou
+    end
   end
 end
